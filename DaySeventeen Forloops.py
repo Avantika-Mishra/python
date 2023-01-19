4b2cf6cb2b8bb61ed52373f5f533e33374fde198
Avantika Mishra <96308161+Avantika-Mishra@users.noreply.github.com>
2023-01-19 19:09:52 +0530

    Add files via upload

---
+++ DaySeventeen Forloops.py
@@ -0,0 +1,17 @@
+name = 'Avantika Mishra'
+for i in name:
+    print(i, end=", ")
+    if(i=="a"):
+      print("Something Special")
+
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+      print(i)
+
+for k in range(20):
+    print(k)
+
+for k in range(4,9):
+    print(k)
