8ea6b3a6fbdb6bad598c06f42be1ce816b270f48
Avantika Mishra <96308161+Avantika-Mishra@users.noreply.github.com>
2023-01-20 20:01:40 +0530

    Add files via upload

---
+++ DayEighteenwhileloops.py
@@ -0,0 +1,27 @@
+''' ''' for i in range(3):
+      print(i)
+
+i= int(input("Enter the number: "))
+while(i<5):
+  i= int(input("Enter the number: "))
+  print(i)
+  i = i + 1
+
+print("Done with loop")
+
+count = -5
+while (count > 0):
+  print(count)
+  count = count - 1
+else:
+  print("Counter is 0") '''
+  
+  
+   '''
+  
+i=0
+while True:
+  print(i)
+  i=i+1
+  if(i%100==0):
+    break
---
+++ DayNineteenBreak&Continue.py
@@ -0,0 +1,19 @@
+# for i in range(15):
+#   print("5 X", i+1 , "=" , 5*i+1)
+#   if(i==10):
+#     break
+
+# print("Out of the loops")
+
+# for i in range(15):
+#   if(i==10):
+#     break
+#   print("5 X", i+1 , "=" , 5*(i+1))
+ 
+# print("Out from the loops")
+
+for i in range(15):
+  if(i==10):
+    print("Skip the Iteration")
+    continue
+  print("5 X", i , "=" , 5*i)
