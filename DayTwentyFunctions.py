9d87bcebc310f7520591d630663d7eafeee94282
Avantika Mishra <96308161+Avantika-Mishra@users.noreply.github.com>
2023-01-23 19:50:45 +0530

    Add files via upload

---
+++ DayTwentyFunctions.py
@@ -0,0 +1,28 @@
+def calculateGmean(a,b):
+      mean = (a*b)/(a+b)
+      print(mean)
+  
+a=9
+b=5
+calculateGmean(a,b)
+# gmean = (a*b)/(a+b)
+# print(gmean)
+
+c=89
+d=15
+calculateGmean(c,d)
+# gmean1 = (c*d)/(c+d)
+# print(gmean1)
+
+def isGreater(a,b):
+      if(a>b):
+          print("First is greater")
+      else:
+          print("Second is greater or equal")
+
+a=9
+b=8
+isGreater(a,b)
+c=8
+d=15
+isGreater(c,d)
